39835cd0f627c58965b07a457e808e7b5cf78c42
Dhiraj223 <jha.dhiraj22803@gmail.com>
2024-07-26 13:21:57 +0530

    Hope Man Please

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hope it works Now Man Please!'
+    return 'Hope it works Now Man Pleasee!'
 
 if __name__ == '__main__':
     app.run()
