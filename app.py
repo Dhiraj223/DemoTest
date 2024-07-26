e2765c113e40f2149053d496c300b72b460d3e26
Dhiraj223 <jha.dhiraj22803@gmail.com>
2024-07-26 12:56:18 +0530

    Hope

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'My New World Dhiraj Jha Try Last Hope It works!'
+    return 'Hope it works!'
 
 if __name__ == '__main__':
     app.run()
